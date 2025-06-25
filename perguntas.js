d1b932ea2620d2185012c6675e1bea35c93dfa25
AnaJulia2009 <ana.alves.ferreira27@escola.pr.gov.br>
2025-06-25 15:59:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lingua inglesa',
+    'Qual a tradução de cause and effect?',
+    '"Causa e efeito"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual palavra em português nao existe em inglês?',
+    '"Saudade"'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a traduçao de fact and opinion?',
+    '"fato e opinião'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    '',
+    'Oi em ingles é HI (RAI)'
+)
